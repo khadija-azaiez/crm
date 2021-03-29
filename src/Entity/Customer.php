5ee506d16faf3d2d21c5999e6d8c68f25afb351a
khadija-azaiez <khadija.azaied0000@gmail.com>
2021-03-29 20:55:13 +0200

    add customer and supplier classes

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -4,7 +4,24 @@
 namespace App\Entity;
 
 
-class Customer
+final class Customer extends Person
 {
+    /** @var int */
+    private $cin;
 
+    /**
+     * @return int
+     */
+    public function getCin(): int
+    {
+        return $this->cin;
+    }
+
+    /**
+     * @param int $cin
+     */
+    public function setCin(int $cin): void
+    {
+        $this->cin = $cin;
+    }
 }
