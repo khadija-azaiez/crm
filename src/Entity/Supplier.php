78c47cdd3d8972ec29a7bdbbc80e7544106a9918
khadija-azaiez <khadija.azaied0000@gmail.com>
2021-03-29 20:55:13 +0200

    add customer and supplier classes

--- src/Entity/Supplier.php
+++ src/Entity/Supplier.php
@@ -4,7 +4,24 @@
 namespace App\Entity;
 
 
-class Supplier
+final class Supplier extends Person
 {
+    /** @var string */
+    private $codeTva;
 
+    /**
+     * @return string
+     */
+    public function getCodeTva(): string
+    {
+        return $this->codeTva;
+    }
+
+    /**
+     * @param string $codeTva
+     */
+    public function setCodeTva(string $codeTva): void
+    {
+        $this->codeTva = $codeTva;
+    }
 }
